5649e30673a9851a0e0f706672634cc747d13a80
lamputt <phucvlbh00630@fpt.edu.vn>
2024-07-09 19:46:49 +0700

    first commit

---
+++ StudentManagement/Data/StudentManagementContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentManagement.Models;
+
+namespace StudentManagement.Data
+{
+    public class StudentManagementContext : DbContext
+    {
+        public StudentManagementContext (DbContextOptions<StudentManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentManagement.Models.Departments> Departments { get; set; } = default!;
+        public DbSet<StudentManagement.Models.Courses> Courses { get; set; } = default!;
+        public DbSet<StudentManagement.Models.Roles> Roles { get; set; } = default!;
+    }
+}
